069f7b2730162fe36b79603f17b9aad6c775268e
Mats Johannesen <mats@matsj.net>
2016-03-16 22:38:57 +0100

    Added yr-weather

---
+++ ajax/fetchWeather.php
@@ -0,0 +1,18 @@
+<?php
+    $xml=simplexml_load_file("http://www.yr.no/sted/Norge/Oslo/Oslo/Ammerud/varsel_time_for_time.xml") or die("Error: Cannot create object");
+
+    $weatherID = $xml->forecast->tabular->time[0]->symbol['number'];
+    $weatherDesc = $xml->forecast->tabular->time[0]->symbol['name'];
+    $temperature = $xml->forecast->tabular->time[0]->temperature['value'];
+    $windSpeed = $xml->forecast->tabular->time[0]->windSpeed['name'];
+
+    if ($xml->forecast->tabular->time[0]->temperature['unit'] == "celsius"){
+        $tempUnit = "C";
+    } else {
+        $tempUnit = "F";
+    }
+
+
+    echo '<h2><i class="weathericon icon-'.$weatherID.'"></i>'.$temperature.'&deg;'.$tempUnit.'</h2>';
+    echo '<span class="weatherDesc">'.$windSpeed.'</span>';
+?>
